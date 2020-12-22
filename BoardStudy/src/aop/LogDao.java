2f108d8a97a5e890b0ed7d887cf540b053ffdeb1
Jaehyeong Ha <cubeprince@gmail.com>
2020-12-22 23:55:07 +0900

    add logging aop

---
+++ BoardStudy/src/aop/LogDao.java
@@ -0,0 +1,19 @@
+package aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class LogDao {
+	public Object logging(ProceedingJoinPoint joinPoint) throws Throwable {
+		String signatureString = joinPoint.getSignature().toShortString();
+		System.out.println(signatureString + "시작");
+		long start = System.currentTimeMillis();
+		try {
+			Object result = joinPoint.proceed();
+			return result;
+		} finally {
+			long finish = System.currentTimeMillis();
+			System.out.println(signatureString + "종료");
+			System.out.println(signatureString + "실행 시간:" + (finish - start) + "ms");
+		}
+	}
+}
