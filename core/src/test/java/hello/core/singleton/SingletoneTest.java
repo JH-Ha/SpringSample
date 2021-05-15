c3965e965f42a9d0e84fc7e8df26493d71e431bf
Jaehyeong Ha <cubeprince@gmail.com>
2021-05-15 22:40:13 +0900

    add singleton pattern

--- core/src/test/java/hello/core/singleton/SingletoneTest.java
+++ core/src/test/java/hello/core/singleton/SingletoneTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class SingletoneTest {
 
     @Test
@@ -21,5 +23,17 @@ public class SingletoneTest {
         System.out.println(memberService2);
 
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글콘패턴을 적용한 객체 사용")
+    void singletoneServiceTest(){
+        SingletoneService singletoneService1 = SingletoneService.getInstance();
+        SingletoneService singletoneService2 = SingletoneService.getInstance();
+
+        System.out.println(singletoneService1);
+        System.out.println(singletoneService2);
+
+        assertThat(singletoneService1).isSameAs((singletoneService2));
     }
 }
