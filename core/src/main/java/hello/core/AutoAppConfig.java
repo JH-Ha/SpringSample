8d2c20558220336a6c811ecd493152a6c010c82f
Jaehyeong Ha <cubeprince@gmail.com>
2021-05-19 21:02:38 +0900

    component scan targets

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -14,3 +14,13 @@ public class AutoAppConfig {
 
 //@Bean으로 등록한 component 가 하나도 없다
 
+/*
+컴포넌트 스캔 대상
+@Component
+@Controller
+@Service
+@Repository
+@Configuration
+
+애노테이션은 상속관계가 없다. 스프링이 지원하는 것
+ */
