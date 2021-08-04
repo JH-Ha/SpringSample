a35afc5ef8bda445555872917a597f39d9b7cae8
Jaehyeong Ha <cubeprince@gmail.com>
2021-08-05 01:58:29 +0900

    update board.html to show list of articles

--- BoardStudy/src/com/servlet/BoardWriteServlet.java
+++ BoardStudy/src/com/servlet/BoardWriteServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,7 +16,7 @@ import com.entity.User;
 /**
  * Servlet implementation class BoardServlet
  */
-@WebServlet("/write")
+//@WebServlet("/write")
 public class BoardWriteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
